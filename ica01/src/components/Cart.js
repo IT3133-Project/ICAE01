3bf2ff797dfe5e9a34cc308f9090739ae3bfc2e5
Udara Nalawansa <udarakavishka13@gmail.com>
2024-12-03 23:57:13 +0530

    feat: Enhance Cart component to calculate and display grand total

--- ica01/src/components/Cart.js
+++ ica01/src/components/Cart.js
@@ -1,5 +1,9 @@
-export default function Cart() {
-    
+export default function Cart({Item}) {
+    let grandtotal=0;
+    for(let i=0; i< Item.length;i++){
+        grandtotal += Item[i].totalPrice;
+    }
+
     return (
         <div className="table-container">
             <h4 className="card-title">Cart</h4>
@@ -12,14 +16,19 @@ export default function Cart() {
 
                 </thead>
                 {
-                    
+                    Item.map((itm)=>(
+                        <tr>
+                            <td>{itm.name}</td>
+                            <td>{itm.quantity}</td>
+                            <td>{itm.totalPrice}</td>
+                        </tr>
+                    ))
                 }
                 <tr>
                     <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
+                    <td colSpan={2}>{grandtotal}</td>
                 </tr>
             </table>
-           
         </div>
     );
 }
